d228851d9a5a18a7a50aab04a74480aaf2d8602d
Chandan2485 <chandan96kar@gmail.com>
2022-10-04 17:04:26 +0530

    covid

--- CovidData/src/Component/Covid.jsx
+++ CovidData/src/Component/Covid.jsx
@@ -1,13 +1,12 @@
-import { useState, useEffect } from "react";
+import './Covid.css'
+import React,{ useState, useEffect } from "react";
 const axios = require("axios").default;
 
 export function Covid() {
   const [allDate, setAlldate] = useState([]);
   const [allstate, setAllstate] = useState([]);
-
   const [input, setInput] = useState("");
   const [inputType, setinputType] = useState("state");
-
   const [filter, setFilter] = useState({});
 
   useEffect(() => {
@@ -20,7 +19,7 @@ export function Covid() {
   const clickevent = () => {
     if (inputType === "state") {
       let x = allstate.filter((value) => {
-        return value.state === input;
+        return value.state.toLowerCase() === input.toLowerCase();
       });
       setFilter(...x);
     } else {
@@ -32,14 +31,15 @@ export function Covid() {
   };
 
   return (
-    <>
-      <div>
-        <input
+    <div className='main'>
+      <div className='input_div'>
+        <input 
+        className='input'
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <select name="" id="" onChange={(e) => setinputType(e.target.value)}>
+        <select name="" id="select" onChange={(e) => setinputType(e.target.value)}>
           <option value="state">state</option>
           <option value="date">date</option>
         </select>
@@ -47,34 +47,36 @@ export function Covid() {
       </div>
 
 
-      <div>
+      <div className='data'>
         {filter.state || filter.dateymd ? (
           filter.state ? (
-            <div>
-              <p>State:{filter.state}</p>
-              <p>Active:{filter.active}</p>
-              <p>Confirmed:{filter.confirmed}</p>
-              <p>Deaths:{filter.deaths}</p>
-              <p>Deltaconfirmed:{filter.deltaconfirmed}</p>
-              <p>Deltadeaths:{filter.deltadeaths}</p>
-              <p>Deltarecovered:{filter.deltarecovered}</p>
-              <p>Recovered:{filter.recovered}</p>
+            <div className='state_data'>
+              <div><h1>State</h1><p>{filter.state}</p></div>
+              <div><h1>Active</h1><p>{filter.active}</p></div>
+              <div><h1>Confirmed</h1><p>{filter.confirmed}</p></div>
+              <div><h1>Deaths</h1><p>{filter.deaths}</p></div>
+              <div><h1>Deltaconfirmed</h1><p>{filter.deltaconfirmed}</p></div>
+              <div><h1>Deltadeaths</h1><p>{filter.deltadeaths}</p></div>
+              <div><h1>Deltarecovered</h1><p>{filter.deltarecovered}</p></div>
+              <div><h1>Recovered</h1><p>{filter.recovered}</p></div>
             </div>
           ) : (
-            <div>
-              <p>Date:{filter.date}</p>
-              <p>Dailyconfirmed:{filter.dailyconfirmed}</p>
-              <p>Dailydeceased:{filter.dailydeceased}</p>
-              <p>Dailyrecovered:{filter.dailyrecovered}</p>
-              <p>Totalconfirmed:{filter.totalconfirmed}</p>
-              <p>Totaldeceased:{filter.totaldeceased}</p>
-              <p>Totalrecovered:{filter.totalrecovered}</p>
+            <div className='date_data'>
+              <div><h1>Date</h1><p>{filter.date}</p></div>
+              <div><h1>Dailyconfirmed</h1><p>{filter.dailyconfirmed}</p></div>
+              <div><h1>Dailydeceased</h1><p>{filter.dailydeceased}</p></div>
+              <div><h1>Dailyrecovered</h1><p>{filter.dailyrecovered}</p></div>
+              <div><h1>Totalconfirmed</h1><p>{filter.totalconfirmed}</p></div>
+              <div><h1>Totaldeceased</h1><p>{filter.totaldeceased}</p></div>
+              <div><h1>Totalrecovered</h1><p>{filter.totalrecovered}</p></div>
             </div>
           )
         ) : (
-          <p>no covid</p>
+          <div>
+            {/* <img src="./pic/corona.jpg" alt="" /> */}
+          </div>
         )}
       </div>
-    </>
+    </div>
   );
 }
