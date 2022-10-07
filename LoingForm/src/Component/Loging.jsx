1d6bd272515313b7e8fad451b22654b1f50885d8
Chandan2485 <chandan96kar@gmail.com>
2022-10-07 17:47:27 +0530

    loging

--- LoingForm/src/Component/Loging.jsx
+++ LoingForm/src/Component/Loging.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./Loging.css";
-import { New } from './New';
+import "./new.css";
+// import { New } from './New';
 
 
 
@@ -8,9 +9,32 @@ import { New } from './New';
 
 export function Loging() {
   const [logingPage, setLogingPage] = useState(true);
-  // const [newUser,setNewUser]=useState(true)
-  //   const [logOutPage, setLogOutPage] = useState(false);
-  // const [userName,setUserName]=useState(true)
+  const [userName,setUserName]=useState('')
+  const [userPass,setUserPass]=useState('')
+ const [storage, setStorage] = useState([]);
+  const [loguserName,setLogUserName]=useState('')
+  const [loguserPass,setLogUserPass]=useState('')
+ 
+
+
+const page=()=>{
+    setLogingPage(true)
+}
+
+const logpag=()=>{
+    setLogingPage(false)
+}
+
+const Register=()=>{
+  let k=localStorage.getItem("storage")
+  console.log(k)
+}
+
+useEffect (()=>{
+    localStorage.setItem('storage',JSON.stringify(storage))
+},[])
+
+
 
   return (
     <>
@@ -28,12 +52,45 @@ export function Loging() {
           ) : (
             <div>
               <span>CHANDAN</span>
-              <button>Logout</button>
+              <button onClick={page}>Logout</button>
             </div>
           )}
         </div>
       </div>
-      <New/>
+
+
+
+      {
+        logingPage?(
+            <div className="new">
+            <div>
+                <h2>Loging</h2>
+                <input type="text" 
+                 onChange={(e)=>setLogUserName(e.target.value)}
+                 value={loguserName}
+                />
+                <input type="password" name="" id="" 
+                 onChange={(e)=>setLogUserPass(e.target.value)}
+                 value={loguserPass}
+                />
+                <button  onClick={logpag} >Loging</button>
+            </div>
+            <div>
+                <h2>Register</h2>
+                <input type="text"
+                 onChange={(e)=>setUserName(e.target.value)}
+                 value={userName}
+                 />
+                <input type="password" name="" id="" 
+                onChange={(e)=>setUserPass(e.target.value)}
+                value={userPass}
+                />
+                <button onClick={Register}>Register</button>
+            </div>
+          </div>
+         ):(<p>loging successfull</p>)
+      } 
+      
     </>
   );
 }
--- LoingForm/src/index.js
+++ LoingForm/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Loging } from './Component/Loging';
-import { New } from './Component/New';
+// import { New } from './Component/New';
 // import './index.css';
 // import {App} from './App';
 
